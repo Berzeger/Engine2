70fc5d792ea9da73d8c7acb44fbd7e3cc82cd0ae
Filip Vondrasek <filip@vondrasek.net>
2019-08-11 12:43:45 +0200

    spdlog

--- Engine2/src/Engine.h
+++ Engine2/src/Engine.h
@@ -1,7 +1,8 @@
 #pragma once
-
 // For use by Engine applications
+
 #include "Engine/Application.h"
+#include "Engine/Log.h"
 
 // Entry point
 #include "Engine/EntryPoint.h"
--- Engine2/src/Engine/EntryPoint.h
+++ Engine2/src/Engine/EntryPoint.h
@@ -7,6 +7,10 @@ extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char ** argv)
 {
+	Engine::Log::Init();
+	ENGINE_CORE_WARN("Initialized the logging subsystem");
+	ENGINE_CLIENT_INFO("Hi!");
+	
 	Engine::Application* app = Engine::CreateApplication();
 	app->Run();
 	delete app;
