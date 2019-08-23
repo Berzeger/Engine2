783c4b2ba37f9854eb556bca7d0c62613dbb0fca
Filip Vondrasek <filip@vondrasek.ne>
2019-08-23 09:16:22 +0200

    GLFW input constants

--- Running/src/Running.h
+++ Running/src/Running.h
@@ -5,6 +5,9 @@
 #include "Running/Log.h"
 #include "Running/Layer.h"
 #include "Running/ImGui/ImGuiLayer.h"
+#include "Running/MouseButtonCodes.h"
+#include "Running/KeyCodes.h"
+#include "Running/Input.h"
 
 // Entry point
 #include "Running/EntryPoint.h"
---
+++ Running/src/Running/KeyCodes.h
@@ -0,0 +1,126 @@
+#pragma once
+
+// from glfw3.h
+/* Printable keys */
+#define RUNNING_KEY_SPACE              32
+#define RUNNING_KEY_APOSTROPHE         39  /* ' */
+#define RUNNING_KEY_COMMA              44  /* , */
+#define RUNNING_KEY_MINUS              45  /* - */
+#define RUNNING_KEY_PERIOD             46  /* . */
+#define RUNNING_KEY_SLASH              47  /* / */
+#define RUNNING_KEY_0                  48
+#define RUNNING_KEY_1                  49
+#define RUNNING_KEY_2                  50
+#define RUNNING_KEY_3                  51
+#define RUNNING_KEY_4                  52
+#define RUNNING_KEY_5                  53
+#define RUNNING_KEY_6                  54
+#define RUNNING_KEY_7                  55
+#define RUNNING_KEY_8                  56
+#define RUNNING_KEY_9                  57
+#define RUNNING_KEY_SEMICOLON          59  /* ; */
+#define RUNNING_KEY_EQUAL              61  /* = */
+#define RUNNING_KEY_A                  65
+#define RUNNING_KEY_B                  66
+#define RUNNING_KEY_C                  67
+#define RUNNING_KEY_D                  68
+#define RUNNING_KEY_E                  69
+#define RUNNING_KEY_F                  70
+#define RUNNING_KEY_G                  71
+#define RUNNING_KEY_H                  72
+#define RUNNING_KEY_I                  73
+#define RUNNING_KEY_J                  74
+#define RUNNING_KEY_K                  75
+#define RUNNING_KEY_L                  76
+#define RUNNING_KEY_M                  77
+#define RUNNING_KEY_N                  78
+#define RUNNING_KEY_O                  79
+#define RUNNING_KEY_P                  80
+#define RUNNING_KEY_Q                  81
+#define RUNNING_KEY_R                  82
+#define RUNNING_KEY_S                  83
+#define RUNNING_KEY_T                  84
+#define RUNNING_KEY_U                  85
+#define RUNNING_KEY_V                  86
+#define RUNNING_KEY_W                  87
+#define RUNNING_KEY_X                  88
+#define RUNNING_KEY_Y                  89
+#define RUNNING_KEY_Z                  90
+#define RUNNING_KEY_LEFT_BRACKET       91  /* [ */
+#define RUNNING_KEY_BACKSLASH          92  /* \ */
+#define RUNNING_KEY_RIGHT_BRACKET      93  /* ] */
+#define RUNNING_KEY_GRAVE_ACCENT       96  /* ` */
+#define RUNNING_KEY_WORLD_1            161 /* non-US #1 */
+#define RUNNING_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define RUNNING_KEY_ESCAPE             256
+#define RUNNING_KEY_ENTER              257
+#define RUNNING_KEY_TAB                258
+#define RUNNING_KEY_BACKSPACE          259
+#define RUNNING_KEY_INSERT             260
+#define RUNNING_KEY_DELETE             261
+#define RUNNING_KEY_RIGHT              262
+#define RUNNING_KEY_LEFT               263
+#define RUNNING_KEY_DOWN               264
+#define RUNNING_KEY_UP                 265
+#define RUNNING_KEY_PAGE_UP            266
+#define RUNNING_KEY_PAGE_DOWN          267
+#define RUNNING_KEY_HOME               268
+#define RUNNING_KEY_END                269
+#define RUNNING_KEY_CAPS_LOCK          280
+#define RUNNING_KEY_SCROLL_LOCK        281
+#define RUNNING_KEY_NUM_LOCK           282
+#define RUNNING_KEY_PRINT_SCREEN       283
+#define RUNNING_KEY_PAUSE              284
+#define RUNNING_KEY_F1                 290
+#define RUNNING_KEY_F2                 291
+#define RUNNING_KEY_F3                 292
+#define RUNNING_KEY_F4                 293
+#define RUNNING_KEY_F5                 294
+#define RUNNING_KEY_F6                 295
+#define RUNNING_KEY_F7                 296
+#define RUNNING_KEY_F8                 297
+#define RUNNING_KEY_F9                 298
+#define RUNNING_KEY_F10                299
+#define RUNNING_KEY_F11                300
+#define RUNNING_KEY_F12                301
+#define RUNNING_KEY_F13                302
+#define RUNNING_KEY_F14                303
+#define RUNNING_KEY_F15                304
+#define RUNNING_KEY_F16                305
+#define RUNNING_KEY_F17                306
+#define RUNNING_KEY_F18                307
+#define RUNNING_KEY_F19                308
+#define RUNNING_KEY_F20                309
+#define RUNNING_KEY_F21                310
+#define RUNNING_KEY_F22                311
+#define RUNNING_KEY_F23                312
+#define RUNNING_KEY_F24                313
+#define RUNNING_KEY_F25                314
+#define RUNNING_KEY_KP_0               320
+#define RUNNING_KEY_KP_1               321
+#define RUNNING_KEY_KP_2               322
+#define RUNNING_KEY_KP_3               323
+#define RUNNING_KEY_KP_4               324
+#define RUNNING_KEY_KP_5               325
+#define RUNNING_KEY_KP_6               326
+#define RUNNING_KEY_KP_7               327
+#define RUNNING_KEY_KP_8               328
+#define RUNNING_KEY_KP_9               329
+#define RUNNING_KEY_KP_DECIMAL         330
+#define RUNNING_KEY_KP_DIVIDE          331
+#define RUNNING_KEY_KP_MULTIPLY        332
+#define RUNNING_KEY_KP_SUBTRACT        333
+#define RUNNING_KEY_KP_ADD             334
+#define RUNNING_KEY_KP_ENTER           335
+#define RUNNING_KEY_KP_EQUAL           336
+#define RUNNING_KEY_LEFT_SHIFT         340
+#define RUNNING_KEY_LEFT_CONTROL       341
+#define RUNNING_KEY_LEFT_ALT           342
+#define RUNNING_KEY_LEFT_SUPER         343
+#define RUNNING_KEY_RIGHT_SHIFT        344
+#define RUNNING_KEY_RIGHT_CONTROL      345
+#define RUNNING_KEY_RIGHT_ALT          346
+#define RUNNING_KEY_RIGHT_SUPER        347
+#define RUNNING_KEY_MENU               348
