70ea167dfa319ce18fcf3c3f8d95f28db5fb419a
Filip Vondrasek <filip@vondrasek.net>
2021-01-18 16:58:50 +0100

    Square rendering WiP, transforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,15 +2,19 @@
 
 #include <Running.h>
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Running::Layer
 {
 public:
 	ExampleLayer() :
 		Layer("Example"),
 		_camera(-1.6f, 1.6f, -0.9f, 0.9f),
-		_cameraPosition(0.0f)
+		_cameraPosition(0.0f),
+		_squarePosition(0.0f)
 	{
 		_vertexArray = Running::VertexArray::Create();
+		_blueVertexArray = Running::VertexArray::Create();
 
 		float vertices[3 * 7] = {
 			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
@@ -40,6 +44,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjectionMatrix;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -48,7 +53,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjectionMatrix * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -66,7 +71,38 @@ public:
 			}
 		)";
 
+		std::string blueVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			uniform mat4 u_ViewProjectionMatrix;
+			uniform mat4 u_Transform;
+
+			out vec3 v_Position;
+
+			void main() 
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string blueFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+
+			void main() 
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
 		_shader.reset(Running::Shader::Create(vertexSrc, fragmentSrc));
+		_blueShader.reset(Running::Shader::Create(blueVertexSrc, blueFragmentSrc));
 	}
 
 	void OnUpdate(Running::Timestep dt) override
@@ -100,6 +136,24 @@ public:
 			_cameraRotation -= _cameraRotationSpeed * dt;
 		}
 
+		if (Running::Input::IsKeyPressed(RUNNING_KEY_J))
+		{
+			_squarePosition += _squareMoveSpeed * dt;
+		}
+		else if (Running::Input::IsKeyPressed(RUNNING_KEY_L))
+		{
+			_squarePosition -= _squareMoveSpeed * dt;
+		}
+
+		if (Running::Input::IsKeyPressed(RUNNING_KEY_I))
+		{
+			_squarePosition += _squareMoveSpeed * dt;
+		}
+		else if (Running::Input::IsKeyPressed(RUNNING_KEY_K))
+		{
+			_squarePosition -= _squareMoveSpeed * dt;
+		}
+
 		Running::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
 		Running::RenderCommand::Clear();
 
@@ -108,14 +162,19 @@ public:
 
 		Running::Renderer::BeginScene(_camera);
 
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), _squarePosition);
+
 		Running::Renderer::Submit(_shader, _vertexArray);
+		Running::Renderer::Submit(_blueShader, _vertexArray);
 
 		Running::Renderer::EndScene();
 	}
 	
 private:
 	std::shared_ptr<Running::Shader> _shader;
+	std::shared_ptr<Running::Shader> _blueShader;
 	std::shared_ptr<Running::VertexArray> _vertexArray;
+	std::shared_ptr<Running::VertexArray> _blueVertexArray;
 
 	Running::OrthographicCamera _camera;
 	glm::vec3 _cameraPosition;
@@ -123,6 +182,9 @@ private:
 
 	float _cameraRotation = 0.0f;
 	float _cameraRotationSpeed = 200.0f;
+
+	glm::vec3 _squarePosition;
+	float _squareMoveSpeed = 10.0f;
 };
 
 class Sandbox : public Running::Application
